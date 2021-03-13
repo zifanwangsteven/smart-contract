63ab13dee2cd81f0f4d3895f5df7b234e4d99daf
Zifan Wang <zifanwangsteven@gmail.com>
2021-03-13 10:35:29 +0800

    updated security checkings

--- contract.py
+++ contract.py
@@ -1,17 +1,22 @@
 from pyteal import *
 """Escrow Account for Interest Payment and Par Value Payment"""
-# remember to verify the repayment of par
 
 def EscrowAccount(mgr_add, interest_id, par_id, accepted_payment, closure, par, coupon,
                   holdup_period, begin_round, end_round, total_payment,
                   period, span):
+    max_fee = Int(1000)
+    lease = Bytes("base64", "NPWoaHaDbyyovXHAHoc5AnKskQwfmTm4YK+psfQutvM=")
+
     # for EscrowAccount to receive payments in opt-in interest token
     # arg_id = 0
     optInInterest = And(
         Txn.sender() == Txn.asset_receiver(),
         Txn.type_enum() == TxnType.AssetTransfer,
         Txn.xfer_asset() == Int(interest_id),
-        Txn.asset_amount() == Int(0)
+        Txn.asset_amount() == Int(0),
+        Txn.rekey_to() == Global.zero_address(),
+        Txn.asset_close_to() == Global.zero_address(),
+        Txn.fee() <= max_fee
     )
 
     # for EscrowAccount to receive payments in opt-in par-value token
@@ -20,7 +25,10 @@ def EscrowAccount(mgr_add, interest_id, par_id, accepted_payment, closure, par, 
         Txn.sender() == Txn.asset_receiver(),
         Txn.type_enum() == TxnType.AssetTransfer,
         Txn.xfer_asset() == Int(par_id),
-        Txn.asset_amount() == Int(0)
+        Txn.asset_amount() == Int(0),
+        Txn.rekey_to() == Global.zero_address(),
+        Txn.asset_close_to() == Global.zero_address(),
+        Txn.fee() <= max_fee
     )
 
     # escrow account receives payment_id as payment
@@ -29,7 +37,10 @@ def EscrowAccount(mgr_add, interest_id, par_id, accepted_payment, closure, par, 
         Txn.sender() == Txn.asset_receiver(),
         Txn.type_enum() == TxnType.AssetTransfer,
         Txn.xfer_asset() == Int(accepted_payment),
-        Txn.asset_amount() == Int(0)
+        Txn.asset_amount() == Int(0),
+        Txn.rekey_to() == Global.zero_address(),
+        Txn.asset_close_to() == Global.zero_address(),
+        Txn.fee() <= max_fee
     )
 
     # arg_id = 3
@@ -38,11 +49,15 @@ def EscrowAccount(mgr_add, interest_id, par_id, accepted_payment, closure, par, 
         Txn.type_enum() == TxnType.AssetTransfer,
         Txn.xfer_asset() == Int(accepted_payment),
         Txn.first_valid() > Int(holdup_period),
+        Txn.rekey_to() == Global.zero_address(),
+        Txn.asset_close_to() == Global.zero_address(),
+        Txn.fee() <= max_fee
     )
 
     # user purchases token from escrow
     # arg_id = 4
     purchase =  And(
+        Global.group_size() == Int(3),
         Gtxn[0].type_enum() == TxnType.AssetTransfer,
         Gtxn[0].xfer_asset() == Int(accepted_payment),
         Gtxn[0].asset_amount() % Int(par) == Int(0),
@@ -50,16 +65,23 @@ def EscrowAccount(mgr_add, interest_id, par_id, accepted_payment, closure, par, 
         Gtxn[1].type_enum() == TxnType.AssetTransfer,
         Gtxn[1].xfer_asset() == Int(par_id),
         Gtxn[1].asset_amount() == Gtxn[0].asset_amount() / Int(par),
-        Gtxn[0].sender() == Gtxn[1].asset_receiver(),
+        Gtxn[1].asset_receiver() == Gtxn[0].sender(),
+        Gtxn[1].rekey_to() == Global.zero_address(),
+        Gtxn[1].asset_close_to() == Global.zero_address(),
+        Gtxn[1].fee() <= max_fee,
         Gtxn[2].type_enum() == TxnType.AssetTransfer,
         Gtxn[2].xfer_asset() == Int(interest_id),
         Gtxn[2].asset_amount() == Gtxn[1].asset_amount() * Int(total_payment),
-        Gtxn[0].sender() == Gtxn[2].asset_receiver(),
+        Gtxn[2].asset_receiver() == Gtxn[0].sender(),
+        Gtxn[2].rekey_to() == Global.zero_address(),
+        Gtxn[2].asset_close_to() == Global.zero_address(),
+        Gtxn[2].fee() <= max_fee
     )
 
     # user receives interest from escrow
     # arg_id = 5
     interestPayment =  And(
+        Global.group_size() == Int(3),
         Gtxn[0].type_enum() == TxnType.AssetTransfer,
         Gtxn[0].xfer_asset() == Int(interest_id),
         Gtxn[1].type_enum() == TxnType.AssetTransfer,
@@ -73,19 +95,27 @@ def EscrowAccount(mgr_add, interest_id, par_id, accepted_payment, closure, par, 
         Gtxn[2].first_valid() >= Int(begin_round),
         Gtxn[2].first_valid() % Int(period) == Int(0),
         Gtxn[2].last_valid() == Gtxn[1].first_valid() + Int(span),
-        Gtxn[2].asset_amount() == Gtxn[0].asset_amount() * Int(coupon)
+        Gtxn[2].asset_amount() == Gtxn[0].asset_amount() * Int(coupon),
+        Gtxn[2].lease() == lease,
+        Gtxn[2].rekey_to() == Global.zero_address(),
+        Gtxn[2].asset_close_to() == Global.zero_address(),
+        Gtxn[2].fee() <= max_fee
     )
 
     # user receives par value from escrow
     # arg_id = 6
     parPayment = And(
+        Global.group_size() == Int(2),
         Gtxn[0].type_enum() == TxnType.AssetTransfer,
         Gtxn[0].xfer_asset() == Int(par_id),
         Gtxn[1].type_enum() == TxnType.AssetTransfer,
         Gtxn[1].first_valid() > Int(end_round),
         Gtxn[1].asset_receiver() == Gtxn[0].sender(),
         Gtxn[1].xfer_asset() == Int(accepted_payment),
-        Gtxn[1].asset_amount() == Gtxn[0].asset_amount() * Int(par)
+        Gtxn[1].asset_amount() == Gtxn[0].asset_amount() * Int(par),
+        Gtxn[1].rekey_to() == Global.zero_address(),
+        Gtxn[1].asset_close_to() == Global.zero_address(),
+        Gtxn[1].fee() <= max_fee
     )
 
 
